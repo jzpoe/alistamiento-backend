0b73ff28deff2961ac49a8610cd058ba23f75a8f
jzpoe <pmguez1287@gmail.com>
2024-12-19 21:22:13 -0500

    se configura cors5

--- src/index.js
+++ src/index.js
@@ -6,7 +6,14 @@ const router = require('./routes/routes');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// Configuración de CORS
+const corsOptions = {
+    origin: 'https://alistamiento-front.vercel.app', // Cambia esto al dominio de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+};
+
+app.use(cors(corsOptions)); // Aplica la configuración de CORS
 const port =process.env.PORT;
 
 
