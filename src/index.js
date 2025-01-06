2ba93e7d1379240fd43f05dcf353aaedce027c73
jzpoe <pmguez1287@gmail.com>
2025-01-05 19:37:13 -0500

    se agregan ruta vercel4

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Configuración de CORS
 const corsOptions = {
-  origin: 'https://alistamiento-front.vercel.app/',
+  origin: 'https://alistamiento-front.vercel.app',
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
 };
